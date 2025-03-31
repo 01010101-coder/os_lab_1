d671230cf056f0137de8ed5c43dc1350f131e948
01010101-coder <v.yahnavets@gmail.com>
2025-03-31 17:17:52 +0300

    Created employee structure

---
+++ include/employee.h
@@ -0,0 +1,12 @@
+/* employee.h */
+#ifndef OS_LAB_1_EMPLOYEE_H
+#define OS_LAB_1_EMPLOYEE_H
+
+// Главная структура
+struct employee {
+    int num;
+    char name[10];
+    double hours;
+};
+
+#endif //OS_LAB_1_EMPLOYEE_H
